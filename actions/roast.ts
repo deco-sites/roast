28a811af24f6fdfacbabcfcbf893742d8fcbcce7
igorbrasileiro <brasileiro456@gmail.com>
2023-11-28 18:58:39 -0300

    first try

--- actions/roast.ts
+++ actions/roast.ts
@@ -1,5 +1,7 @@
-import OpenAI from "https://deno.land/x/openai@v4.20.1/mod.ts";
-import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+import OpenAI, {
+  type FileObject,
+} from "https://deno.land/x/openai@v4.20.1/mod.ts";
+import puppeteer, { Page } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 import { AppContext } from "../apps/site.ts";
 
 interface Props {
@@ -32,6 +34,115 @@ const getAssistant = async () => {
     tools: [{ type: "code_interpreter" }, { type: "retrieval" }],
     model: "gpt-4-1106-preview",
   });
+};
+
+const getLinksFromMenu = async (
+  { html, thread, assistant, url, lastMessageId, page }: {
+    url: string;
+    html: FileObject;
+    thread: any;
+    assistant: any;
+    lastMessageId: string | undefined;
+    page: Page;
+  },
+) => {
+  await openai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content:
+        `Give me a javascript code to access all links in the menu of the site. The output should be a markdown`,
+      file_ids: [html.id],
+    },
+  );
+
+  let run = await openai.beta.threads.runs.create(
+    thread.id,
+    {
+      assistant_id: assistant.id,
+      instructions:
+        `You are an ecommerce expert and you need to assess multiple aspects of a website. The user will upload their website's html and you will need to infer the main audience of that website`,
+    },
+  );
+
+  while (run.status !== "completed") {
+    run = await openai.beta.threads.runs.retrieve(
+      thread.id,
+      run.id,
+    );
+
+    console.log("Running status:", run.status);
+
+    await sleep(1e3);
+  }
+
+  const messages = await openai.beta.threads.messages.list(
+    thread.id,
+    { after: lastMessageId, order: "asc" },
+  );
+
+  let _lastMessageId;
+  let code;
+  for (const message of messages.data) {
+    message.role === "assistant" && console.log(message.content);
+
+    _lastMessageId = message.id;
+  }
+
+  try {
+    console.log("code \n\n", code);
+    if (code) {
+      code = /\`\`\`javascript([\s\S]*)\`\`\`/.exec(code)[1];
+
+      console.log("code", code);
+
+      page.evaluate(code);
+    }
+  } catch (e) {
+    console.error("Error", e);
+  }
+
+  return _lastMessageId;
+};
+
+const getSiteAudience = async ({ assistant, html, url, thread }) => {
+  let run = await openai.beta.threads.runs.create(
+    thread.id,
+    {
+      assistant_id: assistant.id,
+      instructions:
+        `You are an ecommerce expert and you need to assess multiple aspects of a website. The user will upload their website's html and you will need to infer the main audience of that website`,
+    },
+  );
+
+  while (run.status !== "completed") {
+    run = await openai.beta.threads.runs.retrieve(
+      thread.id,
+      run.id,
+    );
+
+    console.log("Running status:", run.status);
+
+    await sleep(1e3);
+  }
+
+  const messages = await openai.beta.threads.messages.list(
+    thread.id,
+    { order: "asc" },
+  );
+
+  let lastMessageId;
+  for (const message of messages.data) {
+    console.log(
+      `-> ${message.role} ${
+        message.content.map((c) => c.type === "text" && c.text.value).join("\n")
+      }`,
+    );
+
+    lastMessageId = message.id;
+  }
+
+  return lastMessageId;
 };
 
 const newTab = async (url: string) => {
@@ -75,44 +186,22 @@ const action = async (
     },
   );
 
-  let run = await openai.beta.threads.runs.create(
-    thread.id,
-    {
-      assistant_id: assistant.id,
-      instructions:
-        `You are an ecommerce expert and you need to assess multiple aspects of a website. The user will upload their website's html and you will need to infer the main audience of that website`,
-    },
-  );
+  // let lastMessageId = await getSiteAudience({ assistant, html, thread, url });
 
-  while (run.status !== "completed") {
-    run = await openai.beta.threads.runs.retrieve(
-      thread.id,
-      run.id,
-    );
-
-    console.log("Running status:", run.status);
-
-    await sleep(1e3);
-  }
-
-  const messages = await openai.beta.threads.messages.list(
-    thread.id,
-  );
-
-  for (const message of messages.data.reverse()) {
-    console.log(
-      `-> ${message.role} ${
-        message.content.map((c) => c.type === "text" && c.text.value).join("\n")
-      }`,
-    );
-  }
+  await getLinksFromMenu({
+    assistant,
+    html,
+    thread,
+    url,
+    lastMessageId: undefined,
+    page,
+  });
 };
 
 export default async (props, req, ctx) => {
   try {
-
-    return await action(props, req, ctx)
+    return await action(props, req, ctx);
   } catch (error) {
-    console.error(error)
-  };
+    console.error(error);
   }
+};
