d4a790b7c52dcafe429ad1f0940ad521316d6933
gimenes <tlgimenes@gmail.com>
2023-11-28 19:31:52 -0300

    hack gpt into outputing a formatted json

--- actions/roast.ts
+++ actions/roast.ts
@@ -1,6 +1,7 @@
 import OpenAI from "https://deno.land/x/openai@v4.20.1/mod.ts";
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 import { AppContext } from "../apps/site.ts";
+import { join } from "std/path/mod.ts";
 
 interface Props {
   url: string;
@@ -29,7 +30,58 @@ const getAssistant = async () => {
     name,
     instructions:
       "You are a staff software engineer and lead designer specialized in the fields of frontend development and web development, with a focus on e-commerce. You help retailers improve their site by roasting the experience on their online stores.",
-    tools: [{ type: "code_interpreter" }, { type: "retrieval" }],
+    tools: [{ type: "code_interpreter" }, { type: "retrieval" }, {
+      "type": "function",
+      "function": {
+        "name": "get_website_audience",
+        "description":
+          "Function to get a possible audience for a given ecommerce website",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "audiences": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "properties": {
+                  "name": {
+                    "type": "string",
+                    "description": "Title of a possible website audience",
+                  },
+                  "age-range": {
+                    "type": "string",
+                    "description":
+                      "Age separated by a dash, for instance 25-45",
+                  },
+                  "interests": {
+                    "type": "string",
+                    "description": "What the audience likes to do",
+                  },
+                  "shopping-preferences": {
+                    "type": "string",
+                    "description": "The audience shopping preferences",
+                  },
+                  "potential-products": {
+                    "type": "string",
+                    "description":
+                      "Which products this audience likely will buy",
+                  },
+                },
+                "required": [
+                  "name",
+                  "age-range",
+                  "interests",
+                  "shopping-preferences",
+                  "potential-products",
+                ],
+              },
+            },
+          },
+          "required": ["audiences"],
+        },
+      },
+    }],
+
     model: "gpt-4-1106-preview",
   });
 };
@@ -42,11 +94,23 @@ const newTab = async (url: string) => {
   return page;
 };
 
+const canonalize = (url: string) => {
+  const u = new URL(url);
+
+  const pathname = u.pathname.endsWith(".html")
+    ? u.pathname
+    : join(u.pathname, "index.html");
+
+  return new URL(`${pathname}${u.search}`, u.origin).href;
+};
+
 const action = async (
-  { url }: Props,
+  props: Props,
   _req: Request,
   __ctx: AppContext,
 ) => {
+  const url = canonalize(props.url);
+
   console.log("retrieving web page for", url);
   const page = await newTab(url);
   const data = await page.evaluate(() =>
@@ -54,7 +118,10 @@ const action = async (
   );
   console.log("done");
 
-  //
+  if (!data) {
+    return;
+  }
+
   const assistant = await getAssistant();
 
   const thread = await openai.beta.threads.create();
@@ -66,11 +133,19 @@ const action = async (
     },
   });
 
+  /**
+   * 1. The Trendy Young Adult Female:
+   - Age: 18-30
+   - Interests: Following the latest fashion trends, enjoying a social lifestyle
+   - Shopping Preferences: Fashionable, urban, and casual wear, as well as sport and fitness apparel
+   - Potential Products: "Estilo Fashion" and "Estilo Urbano" collections, sportswear, and casual wear
+   */
   await openai.beta.threads.messages.create(
     thread.id,
     {
       role: "user",
-      content: `this is the content of my website, accesible at ${url}`,
+      content:
+        `The file anexed is the html of the website accesible at ${url}.`,
       file_ids: [html.id],
     },
   );
@@ -80,11 +155,11 @@ const action = async (
     {
       assistant_id: assistant.id,
       instructions:
-        `You are an ecommerce expert and you need to assess multiple aspects of a website. The user will upload their website's html and you will need to infer the main audience of that website`,
+        `The user uploaded an ecommerce website. Infer the main audience demographics of the website and create between 1 to 5 user category profiles that might browse this website based on this shop's content. Make sure to call get_website_audience function to retrieve the audience`,
     },
   );
 
-  while (run.status !== "completed") {
+  while (run.status === "in_progress" || run.status === 'queued') {
     run = await openai.beta.threads.runs.retrieve(
       thread.id,
       run.id,
@@ -93,6 +168,14 @@ const action = async (
     console.log("Running status:", run.status);
 
     await sleep(1e3);
+  }
+
+  console.log(run)
+
+  if (run.status === "requires_action") {
+    console.log(run.required_action?.submit_tool_outputs.tool_calls);
+
+    return;
   }
 
   const messages = await openai.beta.threads.messages.list(
@@ -110,9 +193,8 @@ const action = async (
 
 export default async (props, req, ctx) => {
   try {
-
-    return await action(props, req, ctx)
+    return await action(props, req, ctx);
   } catch (error) {
-    console.error(error)
-  };
+    console.error(error);
   }
+};
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,11 +2,6 @@ import { AppProps } from "$fresh/server.ts";
 import GlobalTags from "$store/components/GlobalTags.tsx";
 import Theme from "../sections/Theme.tsx";
 
-const sw = () =>
-  addEventListener("load", () =>
-    navigator && navigator.serviceWorker &&
-    navigator.serviceWorker.register("/sw.js"));
-
 function App(props: AppProps) {
   return (
     <>
@@ -18,12 +13,6 @@ function App(props: AppProps) {
 
       {/* Rest of Preact tree */}
       <props.Component />
-
-      {/* Include service worker */}
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
-      />
     </>
   );
 }
